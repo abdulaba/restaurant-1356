cd751d6e9c8e1ee7db550b5b0625797aa088b230
andres ibanez <andresibanezulloa@gmail.com>
2023-11-23 22:13:05 -0300

    vistas index show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,15 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: ["show", "edit", "update", "destroy"]
+  
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
@@ -17,8 +19,22 @@ class RestaurantsController < ApplicationController
   end
 
   def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params 
+    params.require(:restaurant).permit(:name, :address, :city, :photo)
   end
 end
