ca4d799b738bffe5a2a6b261b726b2ebd9a2c856
andres ibanez <andresibanezulloa@gmail.com>
2023-11-23 22:50:30 -0300

    Agregue info al seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create!(name: "Los pollitos dicen", address: "Av el gallinero 123", city: "Pollolandia")
+Restaurant.create!(name: "Los tres cerditos al spiedo y el lobo riendo", address: "Av la granja 234", city: "Chancholandia")
+
